a3276e75801ef79a0c527f99722145d0f16e2f1a
erik radio <radio.erik@gmail.com>
2017-10-11 11:20:08 -0700

    Update get_DOIs.py

--- get_DOIs.py
+++ get_DOIs.py
@@ -1,16 +1,56 @@
 # -*- coding: utf-8 -*-
-import requests, json
+import json
+from urllib.parse import quote
+
+import requests
 
 
-api_endpoint = 'http://api.crossref.org/prefixes/10.2458/works?rows=1000&cursor=*'
 traversed_ids = []
 
 # THIS WILL LOAD EVERY RECORD INTO RAM AT ONCE /disclaimer
 
 
+def debug_request_error(resp):
+    debug_dict = {}
+    debug_dict['status_code'] = resp.status_code
+    try:
+        debug_dict['response'] = resp.json()
+    except:
+        try:
+            debug_dict['response'] = str(resp.data())
+        except:
+            debug_dict['response'] = None
+    raise RuntimeError(
+        "An error has occured, report follows:\n{}".format(
+            json.dumps(debug_dict, indent=4)
+        )
+    )
+
+
+def response_200_json(resp):
+    if resp.status_code != 200:
+        debug_request_error(resp)
+    try:
+        rj = resp.json()
+    except:
+        raise RuntimeError(
+            "Response was not JSON!"
+        )
+    return rj
+
+
 def get_it_all(api_endpoint):
     results = []
-    r = requests.get(api_endpoint).json()
+    try:
+        api_resp = requests.get(api_endpoint)
+    except requests.exceptions.ConnectionError:
+        raise RuntimeError(
+            "The GET request failed, the webserver is down or " +
+            "closed the connection on us."
+        )
+    r = response_200_json(api_resp)
+    if r['status'] != "ok":
+        debug_request_error(api_resp)
     # Records that don't have a next cursor don't have the field
     # at all, so use a call to .get with None as the default
     # in order to avoid KeyErrors being raised
@@ -18,6 +58,7 @@ def get_it_all(api_endpoint):
     next_cursor = r.get('message', {}).get('next-cursor', False)
     if next_cursor:
         # Prevent circular paths
+        global traversed_ids
         if next_cursor not in traversed_ids:
             traversed_ids.append(next_cursor)
             print(next_cursor)
@@ -32,7 +73,7 @@ def get_it_all(api_endpoint):
     # Probably don't actually ever do this
     if r.get('message', {}).get('items', False):
         for x in r['message']['items']:
-            results.insert(0, x)
+            results.append(x)
     print(len(results))
     return results
 
@@ -40,11 +81,17 @@ def get_it_all(api_endpoint):
 def construct_url_from_next_cursor(next_cursor):
     # do stuff here
     # return a valid url as a string
-    return api_endpoint[0:-1] + next_cursor
+    api_endpoint = \
+        'http://api.crossref.org/prefixes/10.2458/works?rows=1000&cursor=*'
+    return api_endpoint[0:-1] + quote(next_cursor)
+
 
 def main():
+    api_endpoint = \
+        'http://api.crossref.org/prefixes/10.2458/works?rows=1000&cursor=*'
     with open('results.json', 'w') as f:
-        json.dump(get_it_all(api_endpoint),f)
+        json.dump(get_it_all(api_endpoint), f)
+
 
 if __name__ == "__main__":
     main()
